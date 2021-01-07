89a31eece141c8ccb941b1a306b1f83059720a18
skoogee <skoogee@gmail.com>
2021-01-07 03:07:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-systemctl stop pi-monitor.service
+systemctl stop ubuntu-pihole-monitor.service
 
 echo ""
 
 echo "Downloading files..."
 
-wget https://raw.githubusercontent.com/Bunn/pi_monitor/master/pi-monitor.py
-wget https://raw.githubusercontent.com/Bunn/pi_monitor/master/pi-monitor.service
+wget https://raw.githubusercontent.com/skoogee/pi_monitor/master/ubuntu-pihole-monitor.py
+wget https://raw.githubusercontent.com/skoogee/pi_monitor/master/ubuntu-pihole-monitor.service
 
 echo ""
 
 echo "Removing old files files..."
-rm /usr/local/bin/pi-monitor.py
-rm /etc/systemd/system/pi-monitor.service
+rm /usr/local/bin/ubuntu-pihole-monitor.py
+rm /etc/systemd/system/ubuntu-pihole-monitor.service
 
 echo ""
 
 echo "Moving files..."
-mv pi-monitor.py /usr/local/bin/
-mv pi-monitor.service /etc/systemd/system/
+mv ubuntu-pihole-monitor.py /usr/local/bin/
+mv ubuntu-pihole-monitor.service /etc/systemd/system/
 
 echo ""
 
-echo "Starting pi-monitor service..."
+echo "Starting ubuntu-pihole-monitor service..."
 systemctl daemon-reload
-systemctl enable pi-monitor.service 
-systemctl start pi-monitor.service
+systemctl enable ubuntu-pihole-monitor.service 
+systemctl start ubuntu-pihole-monitor.service
